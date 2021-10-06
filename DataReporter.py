7395240708573b48144310070b5117a6800a4a12
vishwanath <vishwanathreddyaenugu@gmail.com>
2021-10-06 12:47:21 +0100

    commit

--- DataReporter.py
+++ DataReporter.py
@@ -10,8 +10,8 @@ class Report:
         numericalColumns = self.df.select_dtypes(include = ["int64","float64","datetime64[ns]"]).columns 
         print(" Size of the data set is",self.df.shape,"\n")
         for column in self.df.columns:
-            if(self.df[column].unique().shape[0] ==1 ):
-                print("\ncolumn: ",column )
+            if(self.df[column].unique().shape[0] == 1 ):
+                print("\ncolumn: ",column ) 
                 print(column, " has zero variance")
             elif(self.df[column].unique().shape[0] > 1 and self.df[column].unique().shape[0] < 100):
                 print("\ncolumn: ",column )
--- README.md
+++ README.md
@@ -9,3 +9,14 @@ objectives:
 1) Report number of unique values per column
 2) Report percentage of Missing values per column
 3) Report outliers
+
+
+
+Usage: 
+1) Download DataReporter.py
+
+import DataReporter
+report = Report(df)  
+# for jupyter notebooks include " %run -i DataReporter.py"
+report.eda()
+report.missing()
